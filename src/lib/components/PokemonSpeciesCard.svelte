61e95637b2d64aac1deaf1b017c3acc3678bdc2f
Pedro Santiago <santiagopedrodev@gmail.com>
2025-05-14 18:52:33 -0300

    feat: Refactor PokémonSpeciesCard layout and remove placeholder content from main page

--- src/lib/components/PokemonSpeciesCard.svelte
+++ src/lib/components/PokemonSpeciesCard.svelte
@@ -5,6 +5,16 @@
 </script>
 
 <Card.Root class="grid place-items-center border-2 px-4 text-center">
+	<Card.Header>
+		<img
+			src={`https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+			alt="Imagem foda"
+			class="w-60 object-cover transition-transform duration-300 group-hover:scale-[1.2]"
+			loading="lazy"
+			style="view-transition-name: {pokemon.name}-img;"
+		/>
+	</Card.Header>
+
 	<Card.Content>
 		<Card.Title
 			class="text-xl capitalize"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,11 +6,6 @@
 	let { data }: PageProps = $props();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
-
 {#await data.pokemonSpeciesList}
 	<h1>lole</h1>
 {:then pokemonSpeciesList}
